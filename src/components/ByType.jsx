aea6ac706869c07340ee86c0e24c24602737f08d
Nombre Apellido <email@dominio.com>
2024-02-23 17:01:57 -0500

    alesco second

--- src/components/ByType.jsx
+++ src/components/ByType.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
-import { Dropdown } from 'semantic-ui-react'; 
+// import { Dropdown } from 'semantic-ui-react'; 
 
 const ByType = ({ getByType }) => {
     const [types, setTypes] = useState({});
@@ -20,13 +20,13 @@ const ByType = ({ getByType }) => {
     return (
         <div>
             
-            <Dropdown
+            {/* <Dropdown
                 placeholder="Select Type"
                 // fluid
                 // selection
                 options={options}
                 onChange={(e, data) => getByType(data.value)}
-            />
+            /> */}
         </div>
     );
 };
